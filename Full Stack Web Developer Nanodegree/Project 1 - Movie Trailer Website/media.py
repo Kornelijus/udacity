1095ccb9676543709821fc87256370da0063c84d
Kornelijus <kornelijus.github@gmail.com>
2017-05-30 22:48:40 +0300

    Adding media.py

---
+++ Full Stack Web Developer Nanodegree/Project 1 - Movie Trailer Website/media.py
@@ -0,0 +1,6 @@
+class Movie():
+    #initializing Movie class with needed arguments
+    def __init__(self, movie_title, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
